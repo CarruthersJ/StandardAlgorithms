1014cf89192fd0f4de8da7eda53ecbbe35226ee5
James Carruthers <carruthers.j@etoncollege.org.uk>
2022-11-05 11:22:42 +0000

    Creating an added function

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
+func randomFunction() -> Int {
+    var total = Int()
+    for _ in 1...4 {
+        let randomNumber = Int.random(in: 1...3)
+        total += randomNumber
+    }
+    return total
+}
+
 print("Hello, Jamie!")
 
+let question = "How are you today"
+let message = "\(question)?"
+
+print(message)
+
+
+print(randomFunction())
